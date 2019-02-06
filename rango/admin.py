03d06bc19d9e3097325093c741f382e23821e405
Stephen Considine <2324936c@student.gla.ac.uk>
2019-02-06 21:39:04 +0000

    chapter 5 update

--- rango/admin.py
+++ rango/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
 
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
 
 
 # Register your models here.
