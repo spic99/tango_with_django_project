62c9604e9bde87923f0b685dfc168ab4ee9b1570
Stephen Considine <2324936c@student.gla.ac.uk>
2019-02-06 21:24:19 +0000

    chapter 5

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.contrib import admin
+from django.conf import settings
 from rango import views
 
 urlpatterns =[
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
+    url(r'^admin/', admin.site.urls),
 ]
