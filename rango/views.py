e50f212f72456da840291c5d54d81a82f67c3865
Stephen Considine <2324936c@student.gla.ac.uk>
2019-02-07 12:55:25 +0000

    chapter 6 edited

--- rango/views.py
+++ rango/views.py
@@ -12,7 +12,7 @@ def index(request):
 
     category_list= Category.objects.order_by('-likes')[:5]
     page_list=Page.objects.order_by('-views')[:5]
-    context_dict = {'categories':category_list, 'views':page_list}
+    context_dict = {'categories':category_list, 'pages':page_list}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
