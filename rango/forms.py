7e6f37bf858d896c0121a1b18c05a04d5817b5db
Stephen Considine <2324936c@student.gla.ac.uk>
2019-02-08 15:16:17 +0000

    chapter 7, hopefully fixed

--- rango/forms.py
+++ rango/forms.py
@@ -32,8 +32,8 @@ class PageForm(forms.ModelForm):
     def clean(self):
         cleaned_data=self.cleaned_data
         url=cleaned_data.get('url')
-        if url and not url.startswith('http//'):
-            url='http//'+ url
+        if url and not url.startswith('http://'):
+            url='http://'+ url
             cleaned_data['url']=url
 
             return cleaned_data
